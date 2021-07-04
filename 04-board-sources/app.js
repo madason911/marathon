3d3cdedef4c97a891f3bb34cc93216bc9232ce46
madason911 <31772534+madason911@users.noreply.github.com>
2021-07-04 15:46:28 +0300

    05-aim-game

--- 04-board-sources/app.js
+++ 04-board-sources/app.js
@@ -38,8 +38,8 @@ function getRandomColor() {
 	const LETTERS = '0123456789ABCDEF';
 	let color = '#';
 	for (let i = 0; i < 6; i++) {
-	  color += LETTERS[Math.floor(Math.random() * 16)];
+		color += LETTERS[Math.floor(Math.random() * 16)];
 	}
 	// console.log (color);
 	return color;
-  }
+}
